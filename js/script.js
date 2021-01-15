a54c648f0aebb36c8bb31dad0374782a5a7fac7c
Francesco Maurella <francesco.maurella@live.it>
2021-01-15 21:13:24 +0100

    Timer

--- js/script.js
+++ js/script.js
@@ -24,22 +24,27 @@ function getNumsList(num, quantinty){
   return nums;
 }
 
+
 $(document).ready(function(){ // quando il dom è pronto ...
 
   // impostiamo un limite ai numeri partecipanti
   var quantityNums = 5;
   // generiamo un array di 5 numeri casuali
   var randomNums = getNumsList(getRandomNum, quantityNums);
+  // annuncio numeri random
+  alert('Tieni a mente questi ' + quantityNums + ' numeri:\n' + randomNums);
+  alert('Tra 30 secondi dovrai riscriverli');
 
+  // timer
+  var t = 30;
+  setInterval(function(){
+      $('.timer').text(t);
+      t--;
+    }, 940);
 
   setTimeout(function(){
-
-    window.close()
-
     // generiamo un array di 5 numeri scelti dall'user
     var userNums = getNumsList(getUserNum, quantityNums);
-    console.log(userNums);
-
     var commonNums = []; // dichiariamo array numeri comuni
 
     // inseriamovi i numeri in comune tra i due precedenti array
@@ -49,11 +54,18 @@ $(document).ready(function(){ // quando il dom è pronto ...
       }
     }
 
-    alert(commonNums)
-
-  }, 3000);
-
-  alert('Tieni a mente questi ' + quantityNums + ' numeri:\n' + randomNums);
+    var points = commonNums.length // numeri ricordati
+    // esito
+    if (points === quantityNums) {
+      alert('Ottima memoria!\nHai ricordato tutti i numeri:\n' + commonNums);
+    } else if (!points) {
+      alert('Male! non hai ricordato nessun numero.\nUn pò di fosforo?');
+    } else if (points === 1) {
+      alert('Non bene. Hai ricordato solo 1 numero: ' + commonNums);
+    } else {
+      alert('Mmmh, hai ricordato ' + points + ' numeri:\n' + commonNums);
+    }
+  }, 30000);
 
 
 });
